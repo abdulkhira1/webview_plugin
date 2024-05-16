c84b6717e6cf07f0668610d6fa191bfaf78ae49b
ShikharVayuz <shikhar.jain@vayuz.com>
2024-05-16 19:07:25 +0530

    added handling related to native support

--- ios/Classes/WebViewMoFlutter.swift
+++ ios/Classes/WebViewMoFlutter.swift
@@ -3,100 +3,111 @@ import UIKit
 import WebKit
 
 public class WebViewMoFlutterPlugin: NSObject, FlutterPlugin {
- public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "webview_mo_flutter", binaryMessenger: registrar.messenger())
-    let eventChannel = FlutterEventChannel(name: "webview_plugin_events", binaryMessenger: registrar.messenger())
-    let instance = WebViewMoFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-    eventChannel.setStreamHandler(instance)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "webview_mo_flutter", binaryMessenger: registrar.messenger())
+        let eventChannel = FlutterEventChannel(name: "webview_plugin_events", binaryMessenger: registrar.messenger())
+        let instance = WebViewMoFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        eventChannel.setStreamHandler(instance)
 
-    // Initialize the view factory with the plugin as a delegate
-    let factory = WebViewMoFlutterViewFactory(messenger: registrar.messenger(), delegate: instance)
-    registrar.register(factory, withId: "web_view_mo_flutter")
-}
-
-    private var webViewController: WebViewController?
-  private var eventSink: FlutterEventSink?
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "loadUrl":
-        if let args = call.arguments as? [String: Any],
-           let urlString = args["initialUrl"] as? String,
-           let url = URL(string: urlString) {
-            webViewController = WebViewController()
-            webViewController?.url = url
-            webViewController?.delegate = self
-            if let rootVC = UIApplication.shared.keyWindow?.rootViewController {
-                rootVC.present(webViewController!, animated: true, completion: nil)
-            }
-            result(nil)
-        } else {
-            result(FlutterError(code: "INVALID_ARGUMENT", message: "URL is required", details: nil))
-        }
-    case "runJavaScript":
-        if let script = (call.arguments as? [String: Any])?["script"] as? String {
-            webViewController?.webView.evaluateJavaScript(script) { (response, error) in
-                if let error = error {
-                    result(FlutterError(code: "JAVASCRIPT_ERROR", message: error.localizedDescription, details: nil))
-                } else {
-                    result(response)
-                }
-            }
-        } else {
-            result(FlutterError(code: "INVALID_ARGUMENT", message: "JavaScript code is required", details: nil))
-        }
-    default:
-        result(FlutterMethodNotImplemented)
+        // Initialize the view factory with the plugin as a delegate
+        let factory = WebViewMoFlutterViewFactory(messenger: registrar.messenger(), delegate: instance)
+        registrar.register(factory, withId: "web_view_mo_flutter")
     }
-}
+
+    private var eventSink: FlutterEventSink?
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "loadUrl":
+            if let args = call.arguments as? [String: Any],
+               let urlString = args["initialUrl"] as? String {
+                WebViewManager.shared.loadURL(urlString)
+                result(nil)
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "URL is required", details: nil))
+            }
+        case "runJavaScript":
+            if let script = (call.arguments as? [String: Any])?["script"] as? String {
+                WebViewManager.shared.evaluateJavaScript(script, completionHandler: { (response, error) in
+                    if let error = error {
+                        result(FlutterError(code: "JAVASCRIPT_ERROR", message: error.localizedDescription, details: nil))
+                    } else {
+                        result(response)
+                    }
+                })
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "JavaScript code is required", details: nil))
+            }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
 
 extension WebViewMoFlutterPlugin: FlutterStreamHandler {
-  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-    eventSink = events
-    return nil
-  }
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events
+        WebViewManager.shared.delegate = self  // Assuming WebViewManager now includes a delegate
+        return nil
+    }
 
-  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    eventSink = nil
-    return nil
-  }
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        WebViewManager.shared.delegate = nil
+        return nil
+    }
 }
 
 extension WebViewMoFlutterPlugin: WebViewControllerDelegate {
-  func pageDidLoad() {
-    eventSink?("pageLoaded")
-  }
+    func pageDidLoad() {
+        eventSink?("pageLoaded")
+    }
 }
+
 
 protocol WebViewControllerDelegate: AnyObject {
-  func pageDidLoad()
+    func pageDidLoad()
 }
 
-class WebViewController: UIViewController, WKNavigationDelegate {
-  var webView: WKWebView!
-  var url: URL!
-  weak var delegate: WebViewControllerDelegate?
+class WebViewManager: NSObject {
+    static let shared = WebViewManager()
+    var webView: WKWebView!
+    weak var delegate: WebViewControllerDelegate?
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    webView = WKWebView()
-    webView.navigationDelegate = self
-    view = webView
-    webView.load(URLRequest(url: url))
-  }
+    override init() {
+        super.init()
+        // webView = WKWebView()
+         let configuration = WKWebViewConfiguration()
+        configuration.preferences.setValue(true, forKey: "developerExtrasEnabled") // Enables the inspector
+         self.webView = WKWebView(frame: .zero)
+        webView.navigationDelegate = self
+    }
 
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-   let closeButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(close))
-   navigationItem.rightBarButtonItem = closeButton
-  }
+    func getWebView(frame: CGRect) -> WKWebView {
+        webView?.frame = frame
+        return webView!
+    }
 
- @objc func close() {
-   dismiss(animated: true, completion: nil)
- }
+    func loadURL(_ urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        if webView.url != url {
+            webView.load(URLRequest(url: url))
+        } else {
 
-  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    delegate?.pageDidLoad()
-  }
+            // Optionally, notify delegate or log that the URL load was skipped because it's the same as the current one
+
+            print("Load URL skipped as it's the same as the current URL")
+        }
+    }
+
+    func evaluateJavaScript(_ script: String, completionHandler: @escaping (Any?, Error?) -> Void) {
+        webView.evaluateJavaScript(script, completionHandler: completionHandler)
+    }
+}
+
+extension WebViewManager: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        delegate?.pageDidLoad()
+    }
 }
--- ios/Classes/WebViewMoFlutterViewFactory.swift
+++ ios/Classes/WebViewMoFlutterViewFactory.swift
@@ -27,13 +27,14 @@ class WebViewMoFlutter: NSObject, FlutterPlatformView {
     private var delegate: WebViewControllerDelegate?
 
     init(frame: CGRect, viewIdentifier: Int64, args: Any?, messenger: FlutterBinaryMessenger, delegate: WebViewControllerDelegate?) {
-        self.webView = WKWebView(frame: frame)
+        self.webView = WebViewManager.shared.getWebView(frame: frame)
         self.delegate = delegate
         super.init()
         if let argsDict = args as? [String: Any], let urlString = argsDict["initialUrl"] as? String {
             self.url = URL(string: urlString)
             loadUrl()
         }
+        self.webView.navigationDelegate = self
     }
 
     func view() -> UIView {
@@ -41,10 +42,13 @@ class WebViewMoFlutter: NSObject, FlutterPlatformView {
     }
 
     private func loadUrl() {
-        if let url = self.url {
-            let request = URLRequest(url: url)
-            webView.load(request)
-            webView.navigationDelegate = self
+        // Check if Url is same as loaded in the webview
+        // If it is same, then don't load the url again
+        if let url = url, webView.url != url {
+            webView.load(URLRequest(url: url))
+        } else {
+            // Optionally, notify delegate or log that the URL load was skipped because it's the same as the current one
+            print("Load URL skipped as it's the same as the current URL")
         }
     }
 
