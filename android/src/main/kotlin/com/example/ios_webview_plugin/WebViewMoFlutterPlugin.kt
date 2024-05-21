ba7e07e1648a67721abb735b86ae72fdf2cd6136
abdulkhira <abdulkhira7569@gmail.com>
2024-05-21 17:22:08 +0530

    native android webview added

---
+++ android/src/main/kotlin/com/example/ios_webview_plugin/WebViewMoFlutterPlugin.kt
@@ -0,0 +1,79 @@
+package com.example.ios_webview_plugin
+
+import android.content.Context
+import android.util.Log
+import android.webkit.WebView
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.EventChannel
+
+class WebViewMoFlutterPlugin : FlutterPlugin, MethodChannel.MethodCallHandler, EventChannel.StreamHandler, WebViewControllerDelegate {
+
+    private lateinit var methodChannel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private var eventSink: EventChannel.EventSink? = null
+    private lateinit var context: Context
+    private lateinit var webViewManager: WebViewManager
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "webview_mo_flutter").apply {
+            setMethodCallHandler(this@WebViewMoFlutterPlugin)
+        }
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "webview_plugin_events").apply {
+            setStreamHandler(this@WebViewMoFlutterPlugin)
+        }
+        webViewManager = WebViewManager.getInstance(context)
+        flutterPluginBinding.platformViewRegistry.registerViewFactory("web_view_mo_flutter", WebViewMoFlutterViewFactory(flutterPluginBinding.binaryMessenger, this, webViewManager))
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
+        webViewManager.destroyWebView()
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "loadUrl" -> {
+                val urlString = call.argument<String>("initialUrl")
+                if (urlString != null) {
+                    webViewManager.loadURL(urlString)
+                    result.success(null)
+                } else {
+                    result.error("INVALID_ARGUMENT", "URL is required", null)
+                }
+            }
+            "runJavaScript" -> {
+                val script = call.argument<String>("script")
+                if (script != null) {
+                    webViewManager.evaluateJavaScript(script) { response, error ->
+                        if (error != null) {
+                            result.error("JAVASCRIPT_ERROR", error.localizedMessage, null)
+                        } else {
+                            result.success(response)
+                        }
+                    }
+                } else {
+                    result.error("INVALID_ARGUMENT", "JavaScript code is required", null)
+                }
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink) {
+        eventSink = events
+        webViewManager.delegate = this
+    }
+
+    override fun onCancel(arguments: Any?) {
+        eventSink = null
+        webViewManager.delegate = null
+    }
+
+    override fun pageDidLoad() {
+        eventSink?.success("pageLoaded")
+    }
+}
---
+++ android/src/main/kotlin/com/example/ios_webview_plugin/WebViewMoFlutterViewFactory.kt
@@ -0,0 +1,121 @@
+
+package com.example.ios_webview_plugin
+
+import android.content.Context
+import android.util.Log
+import android.webkit.ConsoleMessage
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+class WebViewMoFlutterViewFactory(
+    private val messenger: BinaryMessenger,
+    private val delegate: WebViewControllerDelegate?,
+    private val webViewManager: WebViewManager
+) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        return WebViewMoFlutter(context, viewId, args, messenger, delegate, webViewManager)
+    }
+}
+
+class WebViewMoFlutter(
+    context: Context,
+    viewId: Int,
+    args: Any?,
+    messenger: BinaryMessenger,
+    private val delegate: WebViewControllerDelegate?,
+    private val webViewManager: WebViewManager
+) : PlatformView {
+
+    private val webView: WebView = webViewManager.getOrCreateWebView()
+
+    init {
+        if (args is Map<*, *>) {
+            val initialUrl = args["initialUrl"] as? String
+            initialUrl?.let { webViewManager.loadURL(it) }
+        }
+    }
+
+    override fun getView(): WebView = webView
+
+    override fun dispose() {
+        webViewManager.destroyWebView()
+    }
+}
+
+class WebViewManager private constructor(private val context: Context) {
+
+    var delegate: WebViewControllerDelegate? = null
+    private var webView: WebView? = null
+
+    fun getOrCreateWebView(): WebView {
+        if (webView == null) {
+            Log.d("getOrCreateWebView", "  = = = = = = = = ")
+            webView = WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.javaScriptCanOpenWindowsAutomatically = true
+                webChromeClient = object : WebChromeClient() {
+                    override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                        Log.d("WebViewConsole", "${consoleMessage.message()} at ${consoleMessage.sourceId()}:${consoleMessage.lineNumber()}")
+                        return true
+                    }
+                }
+                webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        delegate?.pageDidLoad()
+                    }
+
+                    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                        return false
+                    }
+                }
+                addJavascriptInterface(object : Any() {
+                    @JavascriptInterface
+                    fun onMessageReceived(message: String) {
+                        Log.d("onMessageReceived", "Message: $message")
+                    }
+                }, "ChartAppDelegate")
+            }
+        }
+        return webView!!
+    }
+
+    fun loadURL(urlString: String) {
+        Log.d("loadURL", "  = = = = = = = = $urlString")
+        webView?.loadUrl(urlString)
+    }
+
+    fun evaluateJavaScript(script: String, completionHandler: (Any?, Throwable?) -> Unit) {
+        webView?.evaluateJavascript(script) { result ->
+            completionHandler(result, null)
+        }
+    }
+
+    fun destroyWebView() {
+        webView?.destroy()
+        webView = null
+    }
+
+    companion object {
+        private var INSTANCE: WebViewManager? = null
+
+        fun getInstance(context: Context): WebViewManager {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: WebViewManager(context.applicationContext).also { INSTANCE = it }
+            }
+        }
+    }
+}
+
+interface WebViewControllerDelegate {
+    fun pageDidLoad()
+}
